8be63e8ea67682d20677dec8d3dd88c3a6bb64a5
Avinash kumar <avinash2y20@gmail.com>
2024-05-19 02:19:29 +0530

    completed signup functionally

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true, // Ensures the host header is updated to the target URL
+        secure: false,      // If you're using HTTP, not HTTPS
+      },
+    },
+  },
   plugins: [react()],
-})
+});
